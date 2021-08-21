8f721b6befdabd8e2718659a7e9a22095f24f131
IsmaelBarcellos <89302911+IsmaelBarcellos@users.noreply.github.com>
2021-08-21 10:27:38 -0300

    Aula_02

---
+++ Scrips/RStudio_Aula_02.R
@@ -0,0 +1,28 @@
+##configurando diretório##
+
+getwd()
+
+## Contribuidores##
+contributors()
+
+print("Bom Sábado")
+
+##Criar um Gráfico##
+plot(1:25)
+
+##Instalar pacotes##
+install.packages("randomForest")
+installed.packages()
+
+##Carregar pacotes##
+library("randomForest")
+
+##Descarregar pacores##
+detach(package:randomForeste)
+
+##Ajuda##
+help("detach")
+
+install.packages("sos")
+
+findFunction("detach")
